e44922580a6a2688fd285a979909a88cd048b6ad
amanrayat <amanrayat32@gmail.com>
2018-11-28 15:31:34 -0500

    setting up the directory

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'trailblazers';
+    const databaseName = 'trailblazersdb';
     var   connectionString = 'mongodb://localhost/';
     connectionString += databaseName;
     mongoose.connect(connectionString);
-}; 
+};
--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.use(bodyParser.urlencoded({extended : true}));
 require ('./db/database')();
 
 //Listen to requests at port 3000
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
