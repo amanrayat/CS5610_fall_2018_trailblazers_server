344f79b81345482c4ba039119c59016f3b963168
amanrayat <amanrayat32@gmail.com>
2018-12-05 16:13:59 -0500

    followers delete

--- schema/userFollower.schema.server.js
+++ schema/userFollower.schema.server.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const userFollowerSchema = mongoose.Schema({
-    _id : Number,
     userId : {type : Number , ref : 'UserModel'},
     followerId : {type : Number , ref : 'UserModel'}
 },{collection: 'userFollower'});
--- services/userFollowers.service.server.js
+++ services/userFollowers.service.server.js
@@ -20,8 +20,20 @@ module.exports = app =>{
             res.send(result)
         })
     };
+    followUser =( req ,res ) =>{
+        followerDao.followUser(req.params['userId'] , req.params['followerId']).then((result )=>{
+            res.send(result)
+        })
+    };
+    RemoveFollowUser = ( req ,res ) =>{
+        followerDao.RemoveFollowUser(req.params['userId'] , req.params['followerId']).then((result )=>{
+            res.send(result)
+        })
+    };
 
     app.get('/api/follow' , findAllFollowers);
+    app.post('/api/user/:userId/follow/:followerId' , followUser);
+    app.delete('/api/user/:userId/unfollow/:followerId' , RemoveFollowUser);
     app.get('/api/user/:userId/follower' , findMyFollowers);
     app.get('/api/user/:userId/following' , findMyFollowing);
 };
