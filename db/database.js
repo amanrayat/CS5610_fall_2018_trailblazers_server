08f19e89f33e81175eea8a8389ca17190d1421ea
Koosh Doctor <kooshdoctor@Kooshs-MacBook-Pro.local>
2018-12-06 18:04:50 -0500

    .gitIgnore Now working

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'trailblazersdb';
-    var   connectionString = 'mongodb://trail:trail1602@ds227664.mlab.com:27664/';
+    var connectionString = 'mongodb://trail:trail1602@ds227664.mlab.com:27664/';
     connectionString += databaseName;
     mongoose.connect(connectionString).then((result) => {
         console.log(result)
