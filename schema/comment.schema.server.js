f42dd61db4aa39794af60d013cf282cdcf7e3368
kodefear <pritmani.s@husky.neu.edu>
2018-12-06 19:37:54 -0500

    Added id to beer and other schemas

--- schema/comment.schema.server.js
+++ schema/comment.schema.server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = mongoose.Schema({
     _id : Number,
     userId : {type : Number , ref : 'UserModel'},
-    beerId : {type:  Number, ref : 'BeerModel'},
+    beerId : {type:  String, ref : 'BeerModel'},
     time : { type : Date, default: Date.now },
     comment : String
 },{collection: 'comment'});
--- schema/likes.schema.server.js
+++ schema/likes.schema.server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const likesSchema = mongoose.Schema({
     _id : Number,
     userId : {type : Number , ref : 'UserModel'},
-    beerId : {type:  Number, ref : 'BeerModel'},
+    beerId : {type:  String, ref : 'BeerModel'},
     time : { type : Date, default: Date.now },
     like : Boolean ,
     rating : Number
