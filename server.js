6dc4a8b029eea586a84be1ebe7e01c7131242742
amanrayat <amanrayat32@gmail.com>
2018-12-10 17:47:46 -0500

    updating the heroku url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(session({
 // app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://lit-earth-30010.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
