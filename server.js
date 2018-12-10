72641faa7c269b60adb8431983035c7ff29d3940
amanrayat <amanrayat32@gmail.com>
2018-12-10 17:25:13 -0500

    adding cors

--- server.js
+++ server.js
@@ -21,15 +21,15 @@ app.use(session({
 }));
 app.use(cors());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header("Access-Control-Allow-Methods",
-//         "GET, POST, PUT, DELETE, OPTIONS");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 app.get('/' , (req , res)=> res.send("The app is up"));
 require('./services/likes.service.server')(app);
