bac0f12e147eb1d44cb27f2ed436c656ae14e305
amanrayat <amanrayat32@gmail.com>
2018-12-10 17:15:55 -0500

    adding cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const session    = require('express-session');
 const express    = require('express');
 const app        = express();
+var cors = require('cors')
 const idleTimeoutSeconds = 30*60*1000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
@@ -18,16 +19,17 @@ app.use(session({
     },
     rolling: true
 }));
+app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Methods",
+//         "GET, POST, PUT, DELETE, OPTIONS");
+//     next();
+// });
 
 app.get('/' , (req , res)=> res.send("The app is up"));
 require('./services/likes.service.server')(app);
